e5e24c4026c99193cd6cef80a3d778689d362840
Julian McNeill <julesdowork@gmail.com>
2020-11-06 18:21:14 -0500

    Dealing Damage

--- RealTimeStrategy/Assets/Scripts/Units/UnitProjectile.cs
+++ RealTimeStrategy/Assets/Scripts/Units/UnitProjectile.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -18,7 +19,23 @@ public class UnitProjectile : NetworkBehaviour
 	public override void OnStartServer()
 	{
 		Invoke(nameof(DestroySelf), destroyAfterSeconds);
-	}	
+	}
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) { return; }
+        }
+
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
+    }
 
     [Server]
     private void DestroySelf()
