2f543d446245027ceb20b489d4982a4944023b59
Julian McNeill <julesdowork@gmail.com>
2020-11-06 20:05:21 -0500

    Winning The Game

---
+++ RealTimeStrategy/Assets/Scripts/UI/GameOverDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using TMPro;
+
+public class GameOverDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverDisplayParent = null;
+    [SerializeField] private TMP_Text winnerNameText = null;
+
+    private void Start()
+    {
+        GameOverHandler.ClientOnGameOver += ClientHandleGameOver;
+    }
+
+    private void OnDestroy()
+    {
+        GameOverHandler.ClientOnGameOver -= ClientHandleGameOver;
+    }
+
+    public void LeaveGame()
+    {
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            NetworkManager.singleton.StopHost();
+        }
+        else
+        {
+            NetworkManager.singleton.StopClient();
+        }
+    }
+
+    private void ClientHandleGameOver(string winner)
+    {
+        winnerNameText.text = $"{winner} has won!";
+
+        gameOverDisplayParent.SetActive(true);
+    }
+}
