bcbad9868295cdcd8e2e6e4027df93d37264e286
Julian McNeill <julesdowork@gmail.com>
2020-11-07 22:26:58 -0500

    Buildings Setup

---
+++ RealTimeStrategy/Assets/Scripts/Camera/FaceCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCamTf;
+
+    private void Start()
+    {
+        mainCamTf = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + mainCamTf.rotation * Vector3.forward,
+            mainCamTf.rotation * Vector3.up);
+    }
+}
